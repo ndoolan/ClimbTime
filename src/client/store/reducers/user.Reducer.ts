6df19d7c260d00c01bef8e3c3492c033aa99a755
Nick Doolan <nickdoolan31@gmail.com>
2024-04-01 13:56:08 -0400

    loginThunk

--- src/client/store/reducers/user.Reducer.ts
+++ src/client/store/reducers/user.Reducer.ts
@@ -1,7 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const initialState = {};
+const initialState = {
+  isLoading: false,
+  currentUser: undefined,
+  error: null,
+  success: false,
+};
+
+interface userCreds {
+  username: string;
+  password: string;
+}
+
+export const login = createAsyncThunk(
+  '/auth/login',
+  async (userCreds: userCreds, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/auth/login',
+        { userCreds: userCreds },
+        { withCredentials: true }
+      );
+      return response.data.userCreds;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.errors);
+    }
+  }
+);
 
 const userReducer = createSlice({
   name: 'users',
