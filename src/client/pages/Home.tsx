87839f021ec80d9eb0a6ccf3594b8312e6b60de8
Nick Doolan <nickdoolan31@gmail.com>
2024-04-16 11:12:17 -0400

    pngSupportUpdate

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -2,11 +2,13 @@ import { Image, Box, Flex } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <Box justifyContent="center" alignItems="center">
-      <Flex>
-        <Image src="./JimmyWebbJPEG.jpg" alt="Climbing Photo"></Image>
-      </Flex>
-    </Box>
+    // <Box justifyContent="center" alignItems="center">
+    //   <Flex>
+    <>
+      <Image src="./JimmyWebbJPEG.jpg" alt="Climbing Photo"></Image>
+    </>
+    //   </Flex>
+    // </Box>
   );
 };
 
