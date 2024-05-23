50a0cd6d02480ffe53401dc3789d4bc9d3795eb6
ndoolan <nickdoolan31@gmail.com>
2024-05-23 14:54:47 -0400

    CompressedMainImages

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     // <Box justifyContent="center" alignItems="center">
     //   <Flex>
     <>
-    {colorMode === "light" ? <Image src="./Joes_Justin.jpg" alt="Climbing Photo"></Image> : <Image src="./Joes_Carson.jpg" alt="Climbing Photo"></Image> }
+    {colorMode === "light" ? <Image src="./Joes_Justin_459kB.jpg" alt="Climbing Photo"></Image> : <Image src="./Joes_Carson_435kB.jpg" alt="Climbing Photo"></Image> }
     </>
     //   </Flex>
     // </Box>
